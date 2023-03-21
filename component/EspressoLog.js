5a4a481f56bdf00a4d512c5fdf15b8309613356f
dufgu888 <dufgu888@gmail.com>
2023-03-22 00:08:44 +0100

    calculator page

--- component/EspressoLog.js
+++ component/EspressoLog.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { View, Text, TextInput, TouchableOpacity, ScrollView, Platform, KeyboardAvoidingView } from 'react-native'
 import { useState, useLayoutEffect } from 'react'
 import { LinearGradient } from 'expo-linear-gradient';
 import { useSafeAreaInsets } from "react-native-safe-area-context";
@@ -16,6 +16,10 @@ const EspressoLog = ({ navigation }) => {
         bean: "",
     });
 
+    const saveData = () => {
+        alert("save");
+    }
+
     return (
         <View style={{
             paddingTop: insets.top,
@@ -23,23 +27,37 @@ const EspressoLog = ({ navigation }) => {
             paddingLeft: insets.left,
             paddingRight: insets.right,
         }}>
-            <LinearGradient
-                colors={['rgb(80, 138, 191)', 'rgb(78, 136, 189)', 'rgb(35, 95, 145)']}
-                className="flex-row p-2 align-top h-[80px]"
-            >
-                <Text className="text-slate-50 text-xl font-semibold">
-                    Bean:
-                </Text>
-                <View>
-                    <TextInput
-                        className="text-slate-50 text-xl pl-2 font-semibold"
-                        name="bean"
-                        multiline
-                        onChangeText={newText => onChange("bean", newText)}
-                        value={formData.bean}
-                    />
-                </View>
-            </LinearGradient>
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? 'padding' : "height"}>
+                <LinearGradient
+                    colors={['rgb(80, 138, 191)', 'rgb(78, 136, 189)', 'rgb(35, 95, 145)']}
+                    className="flex-row p-2 align-top h-[80px]"
+                >
+                    <Text className="text-slate-50 text-xl font-semibold">
+                        Bean:
+                    </Text>
+                    <View>
+                        <TextInput
+                            className="text-slate-50 text-xl pl-2 font-semibold"
+                            name="bean"
+                            multiline
+                            onChangeText={newText => onChange("bean", newText)}
+                            value={formData.bean}
+                        />
+                    </View>
+                </LinearGradient>
+                <ScrollView>
+                    <View className="flex-1">
+                        <View className="w-[50%]">
+
+                        </View>
+                    </View>
+                </ScrollView>
+                <TouchableOpacity className="bg-[#4e94cf] rounded-sm p-3"
+                    onPress={saveData}>
+                    <Text className="text-center font-extrabold text-2xl text-slate-50 rounded-md">SAVE ESPRESSO LOG</Text>
+                </TouchableOpacity>
+            </KeyboardAvoidingView>
         </View>
     )
 }
